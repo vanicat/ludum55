b1ec45c72ed824959159b20639630b880cdd7eab
Rémi Vanicat <vanicat@debian.org>
2024-04-13 23:52:49 +0200

    Moving arrond.

--- game.py
+++ game.py
@@ -6,10 +6,32 @@ import arcadeLDtk
 from consts import *
 
 class Player(arcade.Sprite):
+    game: "Game"
+
     def __init__(self, game:"Game"):
+        self.game = game
         super().__init__(game.world.defs.tilesets["Player"].set[0])
 
     def on_update(self, delta_time: float = 1 / 60) -> None:
+        self.velocity = (0, 0)
+        if arcade.key.W in self.game.keys:
+            self.velocity = (0, 1)
+        if arcade.key.A in self.game.keys:
+            self.velocity = (-1, 0)
+        if arcade.key.S in self.game.keys:
+            self.velocity = (0, -1)
+        if arcade.key.D in self.game.keys:
+            self.velocity = (1, 0)
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.collides_with_list(self.game.cur_scene["Wall"]):
+            self.center_x -= self.change_x
+            self.center_y -= self.change_y
+
+            assert not(self.collides_with_list(self.game.cur_scene["Wall"]))
+    
 
         return super().on_update(delta_time)
 
@@ -18,6 +40,7 @@ class Game(arcade.View):
     world: arcadeLDtk.LDtk
     cur_level: arcadeLDtk.Level
     cur_scene: arcade.Scene
+    keys: set[int]
     
     def __init__(self, window: arcade.Window, world: arcadeLDtk.LDtk) -> None:
         super().__init__(window)
@@ -43,6 +66,7 @@ class Game(arcade.View):
 
 
     def start_level(self, start_level:arcadeLDtk.Level, pos:tuple[float, float]):
+        self.keys = set()
         self.cur_level = start_level
         self.cur_scene = self.cur_level.make_scene(regenerate=True)
         arcade.set_background_color(self.cur_level.bg_color)
@@ -64,3 +88,11 @@ class Game(arcade.View):
         self.cur_scene.on_update(delta_time)
         self.camera_to_player()
         return super().on_update(delta_time)
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        self.keys.add(symbol)
+        return super().on_key_press(symbol, modifiers)
+    
+    def on_key_release(self, symbol: int, _modifiers: int):
+        self.keys.remove(symbol)
+        return super().on_key_release(symbol, _modifiers)
